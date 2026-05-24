9d740293302756ac7e811b78d2e8885141513646
Gunārs Užulis <114655364+Gunars2004@users.noreply.github.com>
2026-05-24 21:23:05 +0300

    Update generate_train_txt.py

--- generate_train_txt.py
+++ generate_train_txt.py
@@ -25,30 +25,30 @@ for i, img_name in enumerate(image_files):
     frame = cv2.imread(os.path.join(IMAGE_FOLDER, img_name))
     h, w, _ = frame.shape
 
-    # 🔥 paņem tikai apakšu
+    
     roi = frame[int(h*0.92):h, :]
 
     gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
 
-    # kontrasts
+
     gray = cv2.convertScaleAbs(gray, alpha=2.5, beta=0)
 
-    # threshold
+ 
     _, thresh = cv2.threshold(gray, 140, 255, cv2.THRESH_BINARY)
     thresh = cv2.bitwise_not(thresh)
 
-    # ====== atrodam "KM" ======
+   
     text = pytesseract.image_to_string(thresh, config='--psm 6')
 
-    # ja neatrod KM → fallback
+   
     if "KM" not in text:
         labels.append(last_valid)
         continue
 
-    # ====== ņem tikai daļu pirms KM ======
+ 
     parts = text.split("KM")[0]
 
-    # izvelk skaitļus
+    
     digits = re.findall(r'\d+', parts)
 
     if digits:
